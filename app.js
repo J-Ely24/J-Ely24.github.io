2608cce8aafdd6683e3df3f73ecd24bdb3eecb46
J-Ely24 <sherifj43@gmail.com>
2024-09-14 15:28:30 +0100

    Couch to Coder Submission

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-    const images = document.querySelectorAll("img");
-
-    for (const image of images){
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then(response => response.json())
-        .then(data => {
-            image.src = data.message
-            image.width = 100;
-            image.height = 100;
-        })
-    }
-
-})
+var popupModal = document.getElementById("popup_modal");
+var body = document.querySelector("body");
+var closePopupModal = document.getElementById("close");
+var popupForm = document.getElementById("form_container");
+var thanks = document.getElementById("thanks");
+// console.log(popupModal)
+closePopupModal.addEventListener("click",closeModal)
+popupForm.addEventListener("submit", submitForm)
+setTimeout(() => {
+    body.classList.add("dark-bg")
+    popupModal.classList.remove("display-none")
+}, 3000);
+function closeModal () {
+    body.classList.remove("dark-bg")
+    popupModal.classList.add("display-none")
+}
+function submitForm (event) {
+    event.preventDefault();
+    thanks.style.display = "block";
+}
